620ba6d52635d406871965c8f1c87c08346e9f0c
Orangejuice724 <xXSniper1313Xx1313@gmail.com>
2013-11-19 19:49:24 +1030

    Code Upload

--- Scripts/Game Modes/Gamemode.cs
+++ Scripts/Game Modes/Gamemode.cs
@@ -1,13 +1,39 @@
-﻿using UnityEngine;
+﻿// using UnityEngine = Is saying that we want to use the unity engine, and all of its ability
+using UnityEngine;
+// using System.Collections = We're not importing System, we're importing the Collections inside of the System file
 using System.Collections;
 
+//public class Gamemode : MonoBehaviour
+// public = is accessable to every script
+// class = creating the brain to a human body
+// Gamemode giving a name to the brain
+// : Monobehaviour (Make sure you add the :(: = extend) otherwise you'll create an error) MonoBehaviour
+// Monobehaviour, You don't really need to understand what MonoBehaviour is... Generally all of your classes are extending (:)
+// Monobehaviour,  MonoBehaviour is just the core code for c#
 public class Gamemode : MonoBehaviour {
 	
+	//I'll explain this after, but essentially for every gamemode we make, we want to create an instance of it here.
 	public static Gamemode tdm = new TeamDeathmatch();
 	
+	//All variables(varibles?) will be added here so they can be accessed by the child classes (Will explain child class soon)
+	//public (Any script can access it (private no script can access it))
+	//When you want to create a number variable, there are four things you can use, Int, Float, Double and long
+	// int = a number without a decimal point e.g 5
+	// float = a number with decimal points (theres a maximum but cant thing of the top of my head haha) 5.546
+	// Double = a number with two decimal places (generally a rounded float, or long) e.g 5.55
+	// Long (dont really use this unless you are creating time) a number with an extremely long decimal point, e.g 5.234664234562345554
+	//what we are doing
+	// we create a public variable, make it an int, give it a name, and set the default value to be 0 then close
+	// the statement with ;
 	public int auScore = 0;
 	public int ruScore = 0;
+	public int gameTime = 0;
 	
+	//this is a constructor. it can be public, private or nothing, I choose nothing for Start and Update, as those are
+	// generic and should never be called from any class. 
+	// a constructor is what actually holds the code, and makes the code do stuff. Without a constructor, the code wont
+	//execute
+	//a void is something that doesn't return anything. so if we did bool, it would either return true or false
 	void Start () {
 	
 	}
--- Scripts/Game Modes/TeamDeathmatch.cs
+++ Scripts/Game Modes/TeamDeathmatch.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// so here we create the class. But instead of extending MonoDevelop, you extend Gamemode.. So the class Gamemode.
+//when you extend a class you get all of the functions(constructors) that are in the class you extend.
+// so as an example this is the child class, and Gamemode is the parents class. Everytime you call a function in here, thats
+// in the gamemode class, its like taking money from your parents.. (shitty metaphor/simile)
 public class TeamDeathmatch : Gamemode {
 	
 	public int maxDeaths = 300;
 	public int AUDeaths = 0;
 	public int RUDeaths = 0;
+	public int maxTime = 3000;
 	
+	// we create this, so we can create this class in the Gamemode class. we add : base() because we want to reffer to the
+	// Gamemode class. so if we do this, so we say base, which means the gamemode.. base(maxTime max time is an int.)
 	public TeamDeathmatch() : base()
 	{
 		
